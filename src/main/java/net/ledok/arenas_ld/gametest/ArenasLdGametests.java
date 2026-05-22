71cfccd3718f00f616aae702f918fc52d09728c9
ledok <supermaster.ua.lviv@gmail.com>
2026-05-22 14:14:04 +0200

    Pr-2

---
+++ src/main/java/net/ledok/arenas_ld/gametest/ArenasLdGametests.java
@@ -0,0 +1,8 @@
+package net.ledok.arenas_ld.gametest;
+
+/**
+ * Gametest entry point. Individual gametests will be added in later phases as
+ * features land (PC-3 gametests, PE-* gametests, etc.).
+ */
+public final class ArenasLdGametests {
+}
---
+++ src/test/java/net/ledok/arenas_ld/SanityTest.java
@@ -0,0 +1,12 @@
+package net.ledok.arenas_ld;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test
+    void sanityCheck() {
+        assertEquals(4, 2 + 2);
+    }
+}
